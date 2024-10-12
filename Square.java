3accbbdf1403ed8fcba1ca864a7d9b0251d65233
NewToCoding26 <liridon992@gmx.de>
2024-10-12 10:52:57 +0200

    added Square.java

---
+++ Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main (String[] args) {
+        int a = Integer.parseInt(args[0]);  
+        int square = a * a;  
+        
+        System.out.println("java Square " +square);  
+    }
+}
