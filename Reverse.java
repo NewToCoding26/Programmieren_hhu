d8bbc916c512234413a27baf09663a101c0f25c5
NewToCoding26 <liridon992@gmx.de>
2024-10-19 12:54:20 +0200

    added Reverse.java and class but code is not finished

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int erstesArgument = Integer.parseInt(args[0]);
+        int zweitesArgument = Integer.parseInt(args[1]);
+        int drittesArgument = Integer.parseInt(args[2]);
+        int viertesArgument = Integer.parseInt(args[3]);
+
+        System.out.println(viertesArgument);
+        System.out.println(drittesArgument);
+        System.out.println(zweitesArgument);
+        System.out.println(erstesArgument);
+    }
+    
+}
