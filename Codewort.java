d41e794ceef277205ec898ff6823121ecc45af03
NewToCoding26 <liridon992@gmx.de>
2024-10-21 12:19:53 +0200

    added Codewort.java and class

---
+++ Codewort.java
@@ -0,0 +1,6 @@
+public class Codewort {
+    public static void main(String[] args) {
+        System.out.println("https://github.com/hhu-progra/helloworld-NewToCoding26/issues/1");
+        System.out.println("mabre");
+    }
+}
