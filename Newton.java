332db174baf5b1d88cebc68fe74f1903b8442243
NewToCoding26 <liridon992@gmx.de>
2024-10-26 11:51:14 +0200

    added Newton.java and class

---
+++ Newton.java
@@ -0,0 +1,31 @@
+public class Newton {
+
+    public static void main(String[] args) {
+        if (args.length !=1) {
+            System.out.println("ERROR: ungültiges Argument");
+            return;
+        }
+
+        double c = Double.parseDouble(args[0]);
+
+        if (c < 0) {
+            System.out.println("ERROR: ungültiges Argument");
+            return;
+        }
+
+        if (c == 0) {
+            System.out.println("0.0");
+            return;
+        }
+
+
+        double t = c;
+
+        while (Math.abs(t * t - c) > 0.0001) {
+            t = (t + c / t) / 2;
+        }
+
+        System.out.println(t);
+    }
+    
+}
