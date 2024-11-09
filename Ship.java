96642ba4167dd84346aaccf0c61c52de8f8ba3e2
NewToCoding26 <liridon992@gmx.de>
2024-11-09 11:34:31 +0100

    added Ship.java and class & StdDraw.java and class

---
+++ Ship.java
@@ -0,0 +1,22 @@
+public class Ship {
+    public static void main(String[] args) {
+        StdDraw.setScale(-10, 10);
+
+        StdDraw.line(-5, 5, 0, 10);    
+        StdDraw.line(0, 10, 5, 5);      
+        StdDraw.line(5, 5, 0, 0);       
+
+        StdDraw.line(0, 0, -5, -5);     
+        StdDraw.line(-5, -5, 0, -10);   
+        StdDraw.line(0, -10, 5, -5);    
+
+        StdDraw.line(-5, 5, 5, -5);     
+        StdDraw.line(5, 5, -5, -5);     
+    }
+
+    // ich wollte ein Graffiti S zeichnen, bin aber daran verzweifelt
+}
+
+
+
+
